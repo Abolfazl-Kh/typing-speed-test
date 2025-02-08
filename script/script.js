5ecb8a80fdb89a8c726da6a6a109a3d41d6db4c7
Abolfazl <abolfazl.software.dev@gmail.com>
2025-02-08 17:48:45 +0330

    Fix navbar positioning and implement toggle for user menu
    Set navbar to fixed position to keep it visible at all times  
    Adjusted z-index values to ensure proper layering  
    Implemented a toggle function for the user menu using JavaScript  
    Improved menu transition for a smoother experience

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 const menuIcon = document.getElementById('menu-icon');
+const userMenu = document.getElementById('user-menu');
 
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('open');
+    userMenu.classList.toggle('d-block');
 });
