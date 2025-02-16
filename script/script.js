f043b4d162cf400ebb5018b6fea86f19fe01c190
Abolfazl <abolfazl.software.dev@gmail.com>
2025-02-16 18:40:45 +0330

    feat: Display a random text on page load
    Added functionality to show a random text from a predefined list when the page loads.

--- script/script.js
+++ script/script.js
@@ -1,7 +1,22 @@
+//texts sorce
+const texts = [
+    "The quick brown fox jumps over the lazy dog.",
+    "Coding is like humor. If you have to explain it, it’s bad.",
+    "Simplicity is the soul of efficiency.",
+    "Life is short. Code more.",
+    "First, solve the problem. Then, write the code.",
+    "Talk is cheap. Show me the code.",
+    "Before software can be reusable it first has to be usable."
+];
 // Get elements
 const menuIcon = document.getElementById('menu-icon');
 const userMenu = document.getElementById('user-menu');
 const userMenuItems = document.querySelectorAll("#user-menu a")
+const textHolder = document.getElementById("random-text")
+const typingBoxElm = document.getElementById("typing-box")
+let mainText = null
+let randomTextNum = Math.floor(Math.random() * texts.length);
+let isFirstType = true
 
 // Toggle menu on click
 menuIcon.addEventListener('click', () => {
@@ -15,3 +30,8 @@ userMenuItems.forEach(link => {
         alert("This section is not available yet!");
     });
 });
+function textFaind() {
+    mainText = texts[randomTextNum]
+    textHolder.innerHTML = mainText
+}
+window.addEventListener('load', textFaind())
